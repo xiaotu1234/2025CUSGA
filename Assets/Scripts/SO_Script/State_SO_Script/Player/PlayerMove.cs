6022d72a94d8dfb9c0fd4c1f3dfb947074974345
Aminos <2497812382@qq.com>
2025-03-22 16:10:42 +0800

    Revert "Merge branch 'enitity' into FSM"
    This reverts commit ef7303d877d4c463bdff5f98e51694be8d3b560e, reversing
    changes made to 9835840a1f44a1c2d70b52d04773550710fee043.

---
+++ Assets/Scripts/SO_Script/State_SO_Script/Player/PlayerMove.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "PlayerMove", menuName = "ScriptableObject/Player/PlayerMove", order = 0)]
+public class PlayerMove : PlayerState
+{
+    #region Movement Settings “∆∂Ø…Ë÷√
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public float jumpForce = 8f;
+    public float gravity = -20f;
+    private Vector3 dir;
+    private CharacterController m_controller;
+    private Vector3 m_velocity;
+    private Transform m_transform;
+    #endregion
+
+    protected override void Awake()
+    {
+        base.Awake();
+        m_controller = m_Player.GetComponent<CharacterController>();
+        m_transform = m_Player.transform;
+    }
+
+    public override void OnEnter()
+    {
+
+    }
+
+    public override void OnUpdate()
+    {
+        HandleMovement();
+        HandleJump();
+        ApplyGravity();
+    }
+    public override void OnExit()
+    {
+
+    }
+
+    private void HandleMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        dir = new Vector3(x, 0, z);
+        if (dir != Vector3.zero)
+        {
+            m_transform.LookAt(m_transform.position + dir);
+        }
+
+        m_controller.Move(dir * moveSpeed * Time.deltaTime);
+    }
+
+    private void HandleJump()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (!m_controller.isGrounded)
+        {
+            m_velocity.y += gravity * Time.deltaTime;
+        }
+        else if (m_velocity.y < 0)
+        {
+            m_velocity.y = -2f;
+        }
+
+        m_controller.Move(m_velocity * Time.deltaTime);
+    }
+}
