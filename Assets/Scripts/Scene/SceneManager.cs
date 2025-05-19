421e7db28b20dce2ac3bb4941e10c84a56e80382
xiaotu1234 <jp2021212994@qmul.ac.uk>
2025-05-19 13:43:48 +0800

    暂停界面

--- Assets/Scripts/Scene/SceneManager.cs
+++ Assets/Scripts/Scene/SceneManager.cs
@@ -6,10 +6,34 @@ public class SceneManager : SingletonMono<SceneManager>
 {
     private bool isPaused = false;
     public GameObject retryUI;
+    [SerializeField] private GameObject pauseMenuUI; // 暂停界面
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; 
+        isPaused = false;
+
+    }
     public void PauseGame()
     {
         if (!isPaused)
         {
+            pauseMenuUI.SetActive(true);
             Time.timeScale = 0f; // 暂停游戏时间
             Cursor.lockState = CursorLockMode.None; // 解锁鼠标
             Cursor.visible = true; // 显示鼠标
